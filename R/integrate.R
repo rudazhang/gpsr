f85e8137b3c8f08f4b3aa3d20871917a35f93915
ZHANG, Ruda <rudazhan@usc.edu>
2021-08-19 12:09:06 -0400

    Add option for impulse input.

--- R/integrate.R
+++ R/integrate.R
@@ -4,21 +4,24 @@
 #' Solves ODE: \eqn{E \dot{x} = A x + b}
 #' @param b stationary input vector
 #' @return a matrix of state vector snapshots
-backwardEuler <- function(E, A, b, dt = timestep, nt = steps) {
+backwardEuler <- function(E, A, b, dt = timestep, nt = steps, impulse = FALSE) {
     n <- nrow(E)
     X <- matrix(NA_real_, n, nt)
     if (is.matrix(b)) b <- as.vector(b)
     hb <- dt * b
     M <- E - dt * A
     X[,1] <- as.vector(Matrix::solve(M, hb))
-    ## constant input
-    for(k in seq(2, nt)) {
-        X[,k] <- as.vector(Matrix::solve(M, E %*% X[,k-1] + hb))
+    if (!impulse) {
+        ## constant input
+        for(k in seq(2, nt)) {
+            X[,k] <- as.vector(Matrix::solve(M, E %*% X[,k-1] + hb))
+        }
+    } else {
+        ## impulse input
+        for(k in seq(2, nt)) {
+            X[,k] <- as.vector(Matrix::solve(M, E %*% X[,k-1]))
+        }
     }
-    ## impulse input
-    ## for(k in seq(2, nt)) {
-    ##     X[,k] <- as.vector(solve(M, E %*% X[,k-1]))
-    ## }
     return(X)
 }
 
